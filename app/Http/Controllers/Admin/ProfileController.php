2d5816076a5c140938e82be4004ffbd751efa95e
EC2 Default User <ec2-user@ip-172-31-17-216.us-east-2.compute.internal>
2020-03-25 11:24:03 +0000

    create project

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,92 @@
+<?php
+namespace App\Http\Controllers\Admin;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Profile;
+use App\ProfileHistory;
+use Carbon\Carbon;
+
+class ProfileController extends Controller
+{
+  public function add()
+  {
+      return view('admin.profile.create');
+  }
+
+  public function create(Request $request)
+  {
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+      $profile = new Profile;
+      $form = $request->all();
+
+      unset($form['_token']);
+ 
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/create');
+  }
+
+
+ // 以下を追記
+  public function index(Request $request)
+  {
+      $cond_name = $request->cond_name;
+      if ($cond_name != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('name', $cond_name)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+  }
+
+
+  public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+
+  public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+       
+
+        unset($profile_form['_token']);
+        unset($profile_form['remove']);
+        $profile->fill($profile_form)->save();
+
+        // 以下を追記
+        $profile_history = new ProfileHistory;
+        $profile_history->profile_id = $profile->id;
+        $profile_history->edited_at = Carbon::now();
+        $profile_history->save();
+
+        return redirect('admin/profile/');
+    }
+    
+ // 以下を追記　　
+  public function delete(Request $request)
+  {
+      // 該当するProfile Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+  }  
+   
+    
+    
+    
+    
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        //if (\App::environment('production')) {
+       
+        //\URL::forceScheme('https');
+    //}
+     \URL::forceScheme('https');
+    
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    
+    
+    
+    
+}
+
+
+
